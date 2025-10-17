0e9b5b4d5e06b5ee7011f160c4e80b922f944144
mahbhlddnhakkh <predator.nn@yandex.ru>
2025-10-18 00:14:20 +0300

    🫠

--- tasks/01-softmax-cpu/kulagin/main.cpp
+++ tasks/01-softmax-cpu/kulagin/main.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+#include <immintrin.h>
+#include <omp.h>
+
 #include <algorithm>
 #include <chrono>
 #include <cmath>
@@ -12,10 +16,6 @@
 #include <string_view>
 #include <vector>
 
-#include <bits/stdc++.h>
-#include <immintrin.h>
-#include <omp.h>
-
 namespace {
 
 __m256 exp256_ps(__m256 x);
@@ -24,7 +24,7 @@ inline float rand_range(float a, float b) {
   return ((b - a) * ((float)rand() / (float)RAND_MAX)) + a;
 }
 
-void calc_row(const float* row, float* row_res, const std::size_t n) {
+void calc_row(const float *row, float *row_res, const std::size_t n) {
   float d = 0.0f;
   for (std::size_t i = 0; i < n; i++) {
     d += std::exp(row[i]);
@@ -35,7 +35,7 @@ void calc_row(const float* row, float* row_res, const std::size_t n) {
   }
 }
 
-void calc_row_simd(const float* row, float* row_res, const std::size_t n) {
+void calc_row_simd(const float *row, float *row_res, const std::size_t n) {
   __m256 m_d = _mm256_setzero_ps();
   __m256 tmp;
   float d = 0.0f;
@@ -51,9 +51,9 @@ void calc_row_simd(const float* row, float* row_res, const std::size_t n) {
   }
 
   m_d = _mm256_hadd_ps(m_d, m_d);
-  tmp = _mm256_permute_ps(m_d, 0b10110001); // 1 0 3 2
+  tmp = _mm256_permute_ps(m_d, 0b10110001);  // 1 0 3 2
   m_d = _mm256_add_ps(m_d, tmp);
-  tmp = _mm256_permute2f128_ps(m_d, m_d, 0b00100001); // 1 and 2
+  tmp = _mm256_permute2f128_ps(m_d, m_d, 0b00100001);  // 1 and 2
   m_d = _mm256_add_ps(m_d, tmp);
   tmp = _mm256_set1_ps(d);
   m_d = _mm256_add_ps(m_d, tmp);
@@ -74,7 +74,7 @@ void calc_row_simd(const float* row, float* row_res, const std::size_t n) {
 
 std::vector<float> make_matrix(std::size_t n) {
   srand(time(0));
-  constexpr float lower = -10.0f, upper = 10.0f; // Arbitrary numbers
+  constexpr float lower = -10.0f, upper = 10.0f;  // Arbitrary numbers
   const std::size_t res_sz = n * n;
   std::vector<float> res(res_sz);
   for (std::size_t i = 0; i < res_sz; i++) {
@@ -89,8 +89,8 @@ std::vector<float> run_sequential(const std::vector<float> &matrix,
     throw std::runtime_error("Matrix size is not n * n");
   }
   std::vector<float> res(n * n);
-  const float* matrix_ptr = matrix.data();
-  float* res_ptr = res.data();
+  const float *matrix_ptr = matrix.data();
+  float *res_ptr = res.data();
   for (std::size_t i = 0; i < n; i++) {
     calc_row(matrix_ptr + i * n, res_ptr + i * n, n);
   }
@@ -102,8 +102,8 @@ std::vector<float> run_openmp(const std::vector<float> &matrix, std::size_t n) {
     throw std::runtime_error("Matrix size is not n * n");
   }
   std::vector<float> res(n * n);
-  const float* matrix_ptr = matrix.data();
-  float* res_ptr = res.data();
+  const float *matrix_ptr = matrix.data();
+  float *res_ptr = res.data();
 #pragma omp parallel for
   for (std::size_t i = 0; i < n; i++) {
     calc_row(matrix_ptr + i * n, res_ptr + i * n, n);
@@ -119,8 +119,8 @@ std::vector<float> run_simd(const std::vector<float> &matrix, std::size_t n) {
     throw std::runtime_error("Must n >= 8");
   }
   std::vector<float> res(n * n);
-  const float* matrix_ptr = matrix.data();
-  float* res_ptr = res.data();
+  const float *matrix_ptr = matrix.data();
+  float *res_ptr = res.data();
   for (std::size_t i = 0; i < n; i++) {
     calc_row_simd(matrix_ptr + i * n, res_ptr + i * n, n);
   }
@@ -136,8 +136,8 @@ std::vector<float> run_openmp_simd(const std::vector<float> &matrix,
     throw std::runtime_error("Must n >= 8");
   }
   std::vector<float> res(n * n);
-  const float* matrix_ptr = matrix.data();
-  float* res_ptr = res.data();
+  const float *matrix_ptr = matrix.data();
+  float *res_ptr = res.data();
 #pragma omp parallel for
   for (std::size_t i = 0; i < n; i++) {
     calc_row_simd(matrix_ptr + i * n, res_ptr + i * n, n);
@@ -254,7 +254,7 @@ int main(int argc, char *argv[]) {
 namespace {
 
 __m256 exp256_ps(__m256 x) {
-// https://stackoverflow.com/questions/48863719
+//  https://stackoverflow.com/questions/48863719
   /* Modified code from this source: https://github.com/reyoung/avx_mathfun
 
    AVX implementation of exp
@@ -281,13 +281,13 @@ __m256 exp256_ps(__m256 x) {
   (this is the zlib license)
 
   */
-/* 
-  To increase the compatibility across different compilers the original code is
-  converted to plain AVX2 intrinsics code without ingenious macro's,
-  gcc style alignment attributes etc.
-  Moreover, the part "express exp(x) as exp(g + n*log(2))" has been
-  significantly simplified. This modified code is not thoroughly tested!
-*/
+  /* 
+    To increase the compatibility across different compilers the original code
+    is converted to plain AVX2 intrinsics code without ingenious macro's, gcc
+    style alignment attributes etc. Moreover, the part
+    "express exp(x) as exp(g+ n*log(2))" has been significantly simplified.
+    This modified code is not thoroughly tested!
+  */
 
   __m256 exp_hi = _mm256_set1_ps(88.3762626647949f);
   __m256 exp_lo = _mm256_set1_ps(-88.3762626647949f);
